d777956497cad09108087b600078d198a64a0c0b
hritikbharucha <72780268+hritikbharucha@users.noreply.github.com>
2024-05-20 12:00:23 -0700

    finished expose section

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,14 @@ function getRecipesFromStorage() {
 	// A9. TODO - Complete the functionality as described in this function
 	//           header. It is possible in only a single line, but should
 	//           be no more than a few lines.
+	// Get the recipes from localStorage
+	let recipes = localStorage.getItem('recipes');
+	// If there are no recipes, return an empty array
+	if (!recipes) {
+	  return [];
+	}
+	// Parse the recipes from JSON string to an array of objects
+	return JSON.parse(recipes);
 }
 
 /**
@@ -35,10 +43,18 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
 	// A10. TODO - Get a reference to the <main> element
+	const main = document.querySelector('main');
 	// A11. TODO - Loop through each of the recipes in the passed in array,
 	//            create a <recipe-card> element for each one, and populate
 	//            each <recipe-card> with that recipe data using element.data = ...
 	//            Append each element to <main>
+	recipes.forEach((recipe) => {
+		const recipeCard = document.createElement('recipe-card');
+		// Populate each <recipe-card> with that recipe data using element.data
+		recipeCard.data = recipe;
+		// Append each element to <main>
+		main.appendChild(recipeCard);
+	});
 }
 
 /**
